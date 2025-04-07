14460df2159769b05612d4aed461a9e4bd56ea60
chris <c6jones720@yahoo.co.uk>
2025-04-07 18:20:16 +0100

    looks liek weve gone back to atari 800 style joystick interaction but it seems to work as required

--- treadmill-client/Assets/JoystickListener.cs
+++ treadmill-client/Assets/JoystickListener.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro.EditorUtilities;
 using UnityEngine;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
+using static encoder;
 
 public class JoystickListener : MonoBehaviour
 {
@@ -12,6 +14,7 @@ public class JoystickListener : MonoBehaviour
     private InputDevice leftController;
     private InputDevice rightController;
     const float MIN_DELTA = 0.01f;
+    const float MOTION_THRESHOLD = 0.25f;
 
     public encoder joyEncoder;
 
@@ -86,20 +89,64 @@ public class JoystickListener : MonoBehaviour
         Debug.Log(statusMsg);
     }
 
-    
 
+    public float interval = 0.1f; // Delay in seconds (500ms by default)
+    private float timeSinceLastCall = 0f; // Tracks time elapsed since the last call
     void Update()
     {
         bool change = false;
         if (foundLeft) { if (UpdateLeftJoypad()) { change = true; } }
         if (foundRight) { if (UpdateRightJoypad()) { change = true; } }
         CheckDevices();
-        if (change)
-        {   //send latest update
+        timeSinceLastCall += Time.deltaTime;
+
+        if (timeSinceLastCall >= interval)
+        {
+            
+            timeSinceLastCall = 0f; // Reset the timer
+        }
+        TransmitState(change);
+    }
+
+    const uint GO = 1;
+    const uint STOP = 0;
+    uint oldState = STOP;
+    uint currentState = STOP;
+    uint leftState = STOP;
+    uint rightState = STOP;
+
+    void TransmitState(bool change)
+    {
+        if ((rightState == STOP) && (leftState == STOP))
+        {
+            currentState = STOP;
+        }
+        else if ((rightState == GO) || (leftState == GO))
+        {
+            currentState = GO;
+        }
+
+        if (oldState != currentState)
+        {
+            oldState = currentState;
+            if (currentState == STOP)
+            {
+                controllerData.LeftController.Joystick.X = 0.0f;
+                controllerData.LeftController.Joystick.Y = 0.0f;
+                controllerData.RightController.Joystick.X = 0.0f;
+                controllerData.RightController.Joystick.Y = 0.0f;
+            }
+            else
+            {
+                controllerData.LeftController.Joystick.X = 1.0f;
+                controllerData.LeftController.Joystick.Y = 0.0f;
+                controllerData.RightController.Joystick.X = 0.0f;
+                controllerData.RightController.Joystick.Y = 0.0f;
+            }
             SendLatestControllerState();
 
-            LeftX = controllerData.LeftController.Joystick.X.ToString("0.00");
-            LeftY = controllerData.LeftController.Joystick.Y.ToString("0.00");
+            LeftX =  controllerData.LeftController.Joystick.X.ToString("0.00");
+            LeftY =  controllerData.LeftController.Joystick.Y.ToString("0.00");
             RightX = controllerData.RightController.Joystick.X.ToString("0.00");
             RightY = controllerData.RightController.Joystick.Y.ToString("0.00");
         }
@@ -113,10 +160,6 @@ public class JoystickListener : MonoBehaviour
 
 
 
-
-
-    private double previousLeftX = 0.0f;
-    private double previousLeftY = 0.0f;
 
     bool UpdateLeftJoypad()
     {
@@ -132,19 +175,20 @@ public class JoystickListener : MonoBehaviour
                 controllerData.LeftController.Joystick.Y = joystickValue.y;
             }
         }
-        
-        if (Math.Abs(controllerData.LeftController.Joystick.X - previousLeftX) > MIN_DELTA ||
-            Math.Abs(controllerData.LeftController.Joystick.Y - previousLeftY) > MIN_DELTA)
+
+
+        if (Math.Abs(controllerData.LeftController.Joystick.Y) > MOTION_THRESHOLD)
         {
-            previousLeftX = controllerData.LeftController.Joystick.X;
-            previousLeftY = controllerData.LeftController.Joystick.Y;
-            bResult = true;
+            leftState = GO;
+        }
+        else 
+        {
+            leftState = STOP;
         }
         return (bResult);
     }
 
-    private double previousRightX = 0.0f;
-    private double previousRightY = 0.0f;
+ 
     bool UpdateRightJoypad()
     {
         bool bResult = false;
@@ -159,14 +203,16 @@ public class JoystickListener : MonoBehaviour
                 controllerData.RightController.Joystick.Y = joystickValue.y;
             }
         }
-        
-        if (Math.Abs(controllerData.RightController.Joystick.X - previousRightX) > MIN_DELTA ||
-            Math.Abs(controllerData.RightController.Joystick.Y - previousRightY) > MIN_DELTA)
+
+        if (Math.Abs(controllerData.RightController.Joystick.X) > MOTION_THRESHOLD)
         {
-            previousRightX = controllerData.RightController.Joystick.X;
-            previousRightY = controllerData.RightController.Joystick.Y;
-            bResult = true;
+            rightState = GO;
         }
+        else
+        {
+            rightState = STOP;
+        }
+
         return (bResult);
     }
 
